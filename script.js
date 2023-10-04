60a9feceb984cb04523b0f5e46016cf05004ab97
rohan7745 <138517784+rohan7745@users.noreply.github.com>
2023-10-05 01:42:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+function convertToRoman(num) {
+    const romanNumerals = [
+        { value: 100000, symbol: "C̅" },
+        { value: 90000, symbol: "X̅C̅" },
+        { value: 50000, symbol: "L̅" },
+        { value: 40000, symbol: "X̅L̅" },
+        { value: 10000, symbol: "X̅" },
+        { value: 9000, symbol: "I̅X̅" },
+        { value: 5000, symbol: "V̅" },
+        { value: 4000, symbol: "I̅V̅" },
+        { value: 1000, symbol: "M" },
+        { value: 900, symbol: "CM" },
+        { value: 500, symbol: "D" },
+        { value: 400, symbol: "CD" },
+        { value: 100, symbol: "C" },
+        { value: 90, symbol: "XC" },
+        { value: 50, symbol: "L" },
+        { value: 40, symbol: "XL" },
+        { value: 10, symbol: "X" },
+        { value: 9, symbol: "IX" },
+        { value: 5, symbol: "V" },
+        { value: 4, symbol: "IV" },
+        { value: 1, symbol: "I" }
+    ];
+
+    let result = "";
+
+    for (const numeral of romanNumerals) {
+        while (num >= numeral.value) {
+            result += numeral.symbol;
+            num -= numeral.value;
+        }
+    }
+
+    return result;
+}
 document.addEventListener("DOMContentLoaded", function () {
     const convertButton = document.getElementById("convertButton");
     const numberInput = document.getElementById("numberInput");
